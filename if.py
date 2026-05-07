6b1babf7122074dfb6c36cdacf9997dc03417004
Ravish_Janne <ravishjanne2005@gmail.com>
2026-05-07 16:12:06 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,51 @@
+"""even or odd"""
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"{num} is an Even number.")
+# else:
+#     print(f"{num} is an Odd number.")
+"""write a program that asks the user for their age and prints "you are eligible to vote" if the age is 18 or more"""
+# age=int(input("Enter the age of the person:"))
+# if age>=18:
+#     print("You are eligible to vote")
+# else:
+#     print("you are not eligible to vote")
+"""the students have above 90 or above grade a,80to89 grade b,70or79 grade c, 60 to 69 grade d, else fail"""
+# marks=int(input("Enter student marks:"))
+# if marks>=90:
+#     print("grade A")
+# elif marks>=80:
+#     print("Grade B")
+# elif marks>=70:
+#     print("Grade C")
+# elif marks>=60:
+#     print("Grade D")
+# else:
+#     print("fail")
+"""Write a program to input a number,if the number is divisible by 3.print 'fizz' if divisible by 5 print 'buzz'if the number is divisible by both 3 and 5 print 'fizz buzz'"""
+# num = int(input("Enter a number: "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("fizz,buzz")
+# elif num % 3 == 0:
+#     print("fizz")
+# elif num % 5 == 0:
+#     print("buzz")
+# else:
+#     print("number is not divisible by both 3 and 5.")
+"""greatest of 3 number using elif in python"""
+# a,b,c=int(input("enter 1st number:")),int(input("enter 2nd number:")),int(input("enter 3rd number:"))
+# if (a >= b) and (a >= c):
+#     print(f"{a} is largest")
+# elif (b >= a) and (b >= c):
+#     print(f"{b} is largest")
+# else:
+#     print(f"{c} is largest")
+"""write a program that checks if a number is positive if it's positive.further check if it's even or odd.if it's not positive.print 'not a positive number' using nested if """
+num = int(input("Enter a number: "))
+if num > 0:
+    if num % 2 == 0:
+        print("The number is positive and even.")
+    else:
+        print("The number is positive and odd.")
+else:
+    print("Not a positive number")
