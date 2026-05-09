ae326b6f1af7a5769b55b96a20531ef6893c9607
Ravish_Janne <ravishjanne2005@gmail.com>
2026-05-09 16:13:55 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,12 @@
+"""*
+   **
+   ***"""
+# for i in range(1, 4):
+#      print('*' * i)
+"""1
+   1 2
+   1 2 3"""
+for i in range(1, 4):
+     for j in range(1, i + 1):
+         print(j, end=" ")
+     print()
